e149e8dec8493ef1af75347bcc5fc08c399efe10
Xch4rt <pab203.guti@gmail.com>
2021-10-21 11:51:30 -0600

    Creando algunas tablas en el query, falta relacionar

--- Query-RubenDario.sql
+++ Query-RubenDario.sql
@@ -16,3 +16,39 @@ create table Eventos(
 	Precio float,
 )
 
+create table Ventas(
+	IdVenta int primary key identity (1,1),
+	Fecha_venta datetime,
+	Cantidad_Boletos int
+)
+
+create table Boleto(
+	IdBoleto int primary key identity (1,1)
+)
+
+create table Detalles_BCosto(
+	IdDBoleto int primary key identity (1,1),
+	Costo_Asiento float,
+	Porcentaje_cliente float,
+	Porcentaje_teatro float
+)
+
+create table Cliente(
+	
+)
+
+create table Persona(
+	NoCedula varchar (17) primary key,
+	Nombres varchar (35),
+	Apellidos varchar (45),
+	Direccion varchar (75),
+	Telefono int,
+	Fecha_Nacimiento datetime
+)
+
+create table Entidad(
+	NoRuc varchar(20) primary key,
+	NoCedula varchar(17),
+	Direccion varchar(75),
+	Telefono int
+)
